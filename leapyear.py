f6dcd93de8f5a05bb4fd43abbaf26892e4a7018a
Sushma.meesala <72196053+sushmameesala@users.noreply.github.com>
2021-12-10 19:49:47 +0530

    Add files via upload

---
+++ leapyear.py
@@ -0,0 +1,10 @@
+def is_leap(year):
+    
+    if((year%4==0) or (year%100!=0) and (year%400==0)):
+        return True
+    else:
+        return False
+        
+
+year=int(input( ))
+print(is_leap(year))
